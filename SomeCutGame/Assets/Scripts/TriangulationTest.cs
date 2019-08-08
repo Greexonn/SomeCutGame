c83e0f3a8ffe784101e129d195e7631df820225f
Nikita <smirnyh_ns@zrnc.ru>
2019-08-08 19:40:10 +0300

    Triangulation improvments

--- SomeCutGame/Assets/Scripts/TriangulationTest.cs
+++ SomeCutGame/Assets/Scripts/TriangulationTest.cs
@@ -15,13 +15,15 @@ public class TriangulationTest : MonoBehaviour
     private NativeArray<float2> _vertices;
     private NativeList<int> _indexesSorted;
 
-    private NativeHashMap<int, int> _edgesLeft, _edgesRight;
+    private NativeHashMap<int, int> _edgesLeft, _edgesRight, _edgesLeftDoubles, _edgesRightDoubles;
 
     private int _tipIndex = 0;
 
+    private bool _onDoubles;
+
     void Start()
     {
-        _renderer.positionCount = _vertexTransforms.Count + 1;
+        _renderer.positionCount = 3;
 
         _vertices = new NativeArray<float2>(_vertexTransforms.Count, Allocator.Persistent);
         _indexesSorted = new NativeList<int>(_vertexTransforms.Count, Allocator.Persistent);
@@ -33,6 +35,8 @@ public class TriangulationTest : MonoBehaviour
 
         _edgesLeft = new NativeHashMap<int, int>(_edgeIndexes.Count / 2, Allocator.Persistent);
         _edgesRight = new NativeHashMap<int, int>(_edgeIndexes.Count / 2, Allocator.Persistent);
+        _edgesLeftDoubles = new NativeHashMap<int, int>(_edgeIndexes.Count / 2, Allocator.Persistent);
+        _edgesRightDoubles = new NativeHashMap<int, int>(_edgeIndexes.Count / 2, Allocator.Persistent);
         for (int i = 0; i < (_edgeIndexes.Count); i += 2)
         {
             _edgesLeft.TryAdd(_edgeIndexes[i], _edgeIndexes[i + 1]);
@@ -45,10 +49,15 @@ public class TriangulationTest : MonoBehaviour
     void OnDestroy()
     {
         //dispose arrays
-        _vertices.Dispose();
-        _edgesLeft.Dispose();
-        _edgesRight.Dispose();
-        _indexesSorted.Dispose();
+        if (_vertices.IsCreated)
+        {
+            _vertices.Dispose();
+            _edgesLeft.Dispose();
+            _edgesRight.Dispose();
+            _edgesLeftDoubles.Dispose();
+            _edgesRightDoubles.Dispose();
+            _indexesSorted.Dispose();
+        }
     }
 
     private IEnumerator Triangulate()
@@ -79,7 +88,7 @@ public class TriangulationTest : MonoBehaviour
         }
 
         //triangulate
-        while ((_indexesSorted.Length - _tipIndex) > 2)
+        while ((_indexesSorted.Length - _tipIndex) > 1)
         {
             GetTriangle(_indexesSorted[_tipIndex]);
             yield return _ret;
@@ -89,14 +98,34 @@ public class TriangulationTest : MonoBehaviour
         _vertices.Dispose();
         _edgesLeft.Dispose();
         _edgesRight.Dispose();
+        _edgesLeftDoubles.Dispose();
+        _edgesRightDoubles.Dispose();
         _indexesSorted.Dispose();
     }
 
     public void GetTriangle(int earTip)
     {
-        //get two other vertices
-        int _vertLeft = _edgesLeft[earTip];
-        int _vertRight = _edgesRight[earTip];
+        //try get two other vertices
+        _onDoubles = false;
+        int _vertLeft;
+        int _vertRight;
+        //try get doubled edges
+        if (_edgesLeftDoubles.TryGetValue(earTip, out _vertLeft) && _edgesRightDoubles.TryGetValue(earTip, out _vertRight))
+        {
+            _onDoubles = true;
+        }
+        //try get common edges
+        else if (!(_edgesLeft.TryGetValue(earTip, out _vertLeft) && _edgesRight.TryGetValue(earTip, out _vertRight)))
+        {
+            _tipIndex++;
+            return;
+        }
+        if (_vertLeft == _vertRight)
+        {
+            _tipIndex++;
+            return;
+        }
+
 
         //find farthes vertex
         int _boundIndex = _indexesSorted.IndexOf(_vertLeft);
@@ -115,8 +144,15 @@ public class TriangulationTest : MonoBehaviour
             //look if vertex is already connected to other vertices (no double edges)
             if (_vertInner == _edgesLeft[_vertLeft])
             {
+                if (_onDoubles)
+                {
+                    _edgesLeftDoubles.Remove(earTip);
+                }
+                else
+                {
+                    _edgesLeft.Remove(earTip);
+                }
                 //remove edges
-                _edgesLeft.Remove(earTip);
                 _edgesRight.Remove(_vertLeft);
                 _edgesLeft.Remove(_vertLeft);
                 //add edges
@@ -151,30 +187,37 @@ public class TriangulationTest : MonoBehaviour
             }
             else //if vertex is not connected to other (create double edges)
             {
-                
-                return;
+                _vertRight = _vertInner;
+                FindTriangle(ref earTip, ref _vertLeft, ref _vertRight);
             }
         }
         else
         {
-            //remove ear tip edges
-            _edgesLeft.Remove(earTip);
-            _edgesRight.Remove(earTip);
+            if (_onDoubles)
+            {
+                _edgesLeftDoubles.Remove(earTip);
+                _edgesRightDoubles.Remove(earTip);
+            }
+            else
+            {
+                //remove ear tip edges
+                _edgesLeft.Remove(earTip);
+                _edgesRight.Remove(earTip);
+                //move ear tip to next vertex
+                _tipIndex++;
+            }
             //remove edges to ear tip
             _edgesLeft.Remove(_vertRight);
             _edgesRight.Remove(_vertLeft);
             //add new edge
             _edgesLeft.TryAdd(_vertRight, _vertLeft);
             _edgesRight.TryAdd(_vertLeft, _vertRight);
-            //move ear tip to next vertex
-            _tipIndex++;
         }
 
         //debug draw triangle
         _renderer.SetPosition(0, _vertexTransforms[earTip].position);
         _renderer.SetPosition(1, _vertexTransforms[_vertLeft].position);
         _renderer.SetPosition(2, _vertexTransforms[_vertRight].position);
-        _renderer.SetPosition(3, _vertexTransforms[earTip].position);
     }
 
     public int FindInnerVertex(int a, int b, int c, int boundIndex)
@@ -206,8 +249,89 @@ public class TriangulationTest : MonoBehaviour
         return -1;
     }
 
-    public void FindTriangle()
+    public void FindTriangle(ref int earTip, ref int vertLeft, ref int vertRight)
     {
+        //find farthes vertex
+        int _boundIndex = _indexesSorted.IndexOf(vertLeft);
+        int _boundIndex2 = _indexesSorted.IndexOf(vertRight);
+        if (_boundIndex2 > _boundIndex)
+        {
+            _boundIndex = _boundIndex2;
+        }
 
+        int _vertInner = FindInnerVertex(earTip, vertLeft, vertRight, _boundIndex);
+
+        //if intersection found cut upper triangle and add extended edges
+        if (_vertInner != -1)
+        {
+            print(_vertInner);
+            //look if vertex is already connected to other vertices (no double edges)
+            if (_vertInner == _edgesLeft[vertLeft])
+            {
+                //remove edges
+                _edgesLeft.Remove(earTip);
+                _edgesRight.Remove(vertLeft);
+                _edgesLeft.Remove(vertLeft);
+                //add edges
+                if (_edgesLeft[_vertInner] == vertLeft)
+                {
+                    _edgesLeft.Remove(_vertInner);
+                    _edgesLeft.TryAdd(_vertInner, _edgesRight[_vertInner]);
+                }
+                _edgesRight.Remove(_vertInner);
+                _edgesRight.TryAdd(_vertInner, earTip);
+                _edgesLeft.TryAdd(earTip, _vertInner);
+                //
+                vertRight = _vertInner;
+                return;
+            }
+            else if (_vertInner == _edgesRight[vertRight])
+            {
+                //remove edges
+                _edgesRight.Remove(earTip);
+                _edgesLeft.Remove(vertRight);
+                _edgesRight.Remove(vertRight);
+                //add edges
+                if (_edgesRight[_vertInner] == vertRight)
+                {
+                    _edgesRight.Remove(_vertInner);
+                    _edgesRight.TryAdd(_vertInner, _edgesLeft[_vertInner]);
+                }
+                _edgesLeft.Remove(_vertInner);
+                _edgesLeft.TryAdd(_vertInner, earTip);
+                _edgesRight.TryAdd(earTip, _vertInner);
+                //
+                vertLeft = _vertInner;
+                return;
+            }
+            else //if vertex is not connected to other (create double edges)
+            {
+                vertRight = _vertInner;
+                FindTriangle(ref earTip, ref vertLeft, ref vertRight);
+                return;
+            }
+        }
+        else
+        {
+            if (_onDoubles)
+            {
+
+            }
+            else
+            {
+                
+            }
+            //add doubles
+            _edgesLeftDoubles.TryAdd(vertRight, vertLeft);
+            _edgesRightDoubles.TryAdd(vertRight, _edgesRight[vertRight]);
+            //remove edges
+            _edgesLeft.Remove(earTip);
+            _edgesRight.Remove(vertLeft);
+            _edgesRight.Remove(vertRight);
+            //edd edges
+            _edgesRight.TryAdd(vertRight, earTip);
+            _edgesLeft.TryAdd(earTip, vertRight);
+            _edgesRight.TryAdd(vertLeft, vertRight);
+        }
     }
 }
